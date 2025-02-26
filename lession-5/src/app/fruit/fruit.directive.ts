1ac3c23a14bfcca65a70b7843cc9a8cc74a0af81
Milan Mazik <mazik1990@hotmail.com>
2025-02-26 20:49:36 +0100

    style change with directives

--- lession-5/src/app/fruit/fruit.directive.ts
+++ lession-5/src/app/fruit/fruit.directive.ts
@@ -1,4 +1,11 @@
-import { Directive, effect, ElementRef, inject, input } from '@angular/core';
+import {
+  Directive,
+  effect,
+  ElementRef,
+  HostListener,
+  inject,
+  input,
+} from '@angular/core';
 import { Fruit } from './fruit.model';
 
 @Directive({
@@ -6,16 +13,35 @@ import { Fruit } from './fruit.model';
 })
 export class FruitDirective {
   fruit = input<Fruit>(undefined, { alias: 'appFruit' });
+
   private readonly element: ElementRef<HTMLElement> = inject(ElementRef);
 
   constructor() {
     effect(() => {
-      const currentFruit = this.fruit();
-
-      this.element.nativeElement.style.backgroundColor = currentFruit!.color;
-      this.element.nativeElement.style.padding = '4px 8px';
-      this.element.nativeElement.style.borderRadius = '4px';
-      this.element.nativeElement.style.textAlign = 'center';
+      this.formatFruit();
     });
+  }
+
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    this.element.nativeElement.style.backgroundColor = 'blue';
+    this.element.nativeElement.style.color = 'white';
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this.formatFruit();
+  }
+
+  formatFruit() {
+    this.element.nativeElement.style.backgroundColor =
+      this.fruit()?.color ?? 'transparent';
+
+    this.element.nativeElement.style.padding = '4px 8px';
+
+    this.element.nativeElement.style.borderRadius = '4px';
+
+    this.element.nativeElement.style.textAlign = 'center';
+    this.element.nativeElement.style.color = 'black';
   }
 }
